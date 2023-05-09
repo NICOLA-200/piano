a544dbe655d8bbc74c5026dadae4327e8339a2e7
NICOLA-200 <fatepepe66@gmail.com>
2023-05-09 15:51:32 +0200

    added the audio

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 const pianoKeys =  document.querySelectorAll(".piano-keys .key")
+const volumeSlider =  document.querySelector(".volume-slider input")
+const keyCheckbox =  document.querySelector(".keys-checkbox input")
 
-const audio = new Audio("tunes/a.wav")
+
+let allKey = [];
+
+const audio = new Audio("tunes/a.wav");
 
 const playTune = (key) => {
      audio.src =`tunes/${key}.wav`
      audio.play();
+
+     const clickedKey = document.querySelector(`[data-key="${key}"]`);
+     clickedKey.classList.add("active");
+     setTimeout(()=>{ 
+          clickedKey.classList.remove("active");
+     }, 150)
 }
 
 pianoKeys.forEach (key => {
-     console.log(key.dataset.key);
+     allKey.push(key.dataset.key)
+     key.addEventListener("click", () => playTune(key.dataset.key));
 })
+
+const showHideKeys = () => {
+     pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
+
+const handleVolume =(e)=> {
+     audio.volume = e.target.value
+}
 
 
 const pressedKey = (e) => {
-      console.log(e);
+     if (allKey.includes(e.key))   playTune(e.key);
 }
 
+volumeSlider.addEventListener("input", handleVolume)
+keyCheckbox.addEventListener("click", showHideKeys)
 document.addEventListener("keydown", pressedKey)
 
 
